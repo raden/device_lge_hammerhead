708d1ccf6511fd316cbed531cba1fceae03808b3
Najmi <najmi.zabidi@gmail.com>
2015-01-13 15:39:46 +0800

    Init files
    Signed-off-by: Najmi <najmi.zabidi@gmail.com>

---
+++ bliss.mk
@@ -0,0 +1,24 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit some common crDroid stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/bliss/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/hammerhead/full_hammerhead.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := hammerhead
+PRODUCT_NAME := bliss_hammerhead
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 5
+PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hammerhead BUILD_FINGERPRINT=google/hammerhead/hammerhead:5.0.1/LRX22C/1602158:user/release-keys PRIVATE_BUILD_DESC="hammerhead-user 5.0.1 LRX22C 1602158 release-keys"
+
+# Enable Torch
+PRODUCT_PACKAGES += Torch
